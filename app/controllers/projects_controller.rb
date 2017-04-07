804aee1428119b318e5057dde26e4fe890d16a79
Firman Cahyandi <f.cahyandi@gmail.com>
2017-04-07 13:54:21 +1000

    user_id field configured

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,7 +25,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
-
+    @project.user_id = current_user.id
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
